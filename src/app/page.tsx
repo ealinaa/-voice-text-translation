4be0e994105cae5f49f52f860f4f9ce992718ecf
ealinaa <alinaadhikari108@gmail.com>
2024-10-07 21:03:33 +0545

    work with textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import Image from "next/image";
+
+import React, {useState, ChangeEvent} from "react";
+import TextArea from '@/Components/Inputs/TextArea'
+
 
 export default function Home() {
+  const [sourceText , setSourceText] = useState<string>("");
   return (
     <div>
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
@@ -16,8 +20,14 @@ export default function Home() {
             <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
               <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
                 <div className="realtive z-10 flex flex-co, space-x-3 border rounded-lg shadow-lg bg-neutral-900  border-neutral-700 shadow-gray-200">
-                  
-
+          <TextArea
+          id= "source-language"
+          value={sourceText}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>)=>{setSourceText(e.target.value);
+          }}
+          placeholder="Source Language"
+          />
+         
                 </div>
               </div>
 
