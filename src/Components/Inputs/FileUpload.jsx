9b92d76ced1843858680f0ace894f7954cd6516e
ealinaa <alinaadhikari108@gmail.com>
2024-10-08 20:57:51 +0545

    work with file upload and button

---
+++ src/Components/Inputs/FileUpload.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { IconPaperClip } from "@tabler/icons-react"
+const FileUpload = ({ handleFileUpload }) => {
+    <label htmlFor="file-Upload" className="cursor-pointer">
+        <IconPaperClip size={21} />
+        <input id="file-upload" type="file" onChange={handleFileUpload} className="hidden" />
+    </label>
+}
+export default FileUpload
